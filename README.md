7399ba6c6f390d381720e62f38a2c007c670b490
gonzobard777 <gonzo.bard@dwfe.ru>
2025-05-05 21:27:12 +0300

    .

---
+++ README.md
@@ -0,0 +1,40 @@
+# Загрузка данных в бд
+
+Через QGIS загрузить в бд postgres+postgis (смотри docker-compose.yaml) содержимое следующих файлов:
+
+- `files/grid.zip`
+- `files/oikonyms.zip`
+
+Еще варианты как можно загрузить данные:
+
+- [Loading Data into PostGIS: An Overview](https://www.crunchydata.com/blog/loading-data-into-postgis-an-overview)
+- geopandas на питоне
+
+# Запуск мартина
+
+Для генерации векторных тайлов используем сервер векторных тайлов Martin. Загрузим это приложение [отсюда](https://github.com/maplibre/martin/releases).
+```shell
+martin postgresql://postgres:password@localhost:5432/oikonyms
+```
+Output:
+```
+...
+[2025-05-05T17:46:06Z INFO  martin::pg::builder] Auto-publishing functions in schemas [] as '{function}' sources
+[2025-05-05T17:46:06Z INFO  martin] Use --save-config to save or print Martin configuration.
+[2025-05-05T17:46:06Z INFO  martin] Martin has been started on 0.0.0.0:3000.
+[2025-05-05T17:46:06Z INFO  martin] Use http://0.0.0.0:3000/catalog to get the list of available sources.
+[2025-05-05T17:46:06Z INFO  martin] Web UI is disabled. Use `--webui enable-for-all` in CLI or a config value to enable it for all connections.
+```
+
+На последней строчке увидим, что каталог слоёв доступен по адресу `http://0.0.0.0:3000/catalog`, то есть по IP адресам компьютера. Мы воспользуемся нашим любимым localhost (другие обычно и недоступны).
+
+Перейдём по адресу `localhost:3000/catalog`, чтобы увидеть доступные наборы векторных тайлов. Их должно быть два по количеству пространственных таблиц в базе данных.
+
+По адресам `localhost:3000/grid` и `localhost:3000/oikonyms` доступны описания наборов векторных тайлов в формате TileJSON. Наиболее существенным в нём является указание адреса, по которому доступны векторные тайлы — `localhost:3000/grid/{z}/{x}/{y}`
+
+> `/catalog`, `/grid`, `/grid/{z}/{x}/{y}` — это всё эндпоинты API, которое для нас автоматически формирует Martin. Он же выполняет нужные серверные функции, за счёт которых мы получаем ответы, обращаясь к этим эндпоинтам. И ничего не пришлось писать самим, как в прошлом упражнении!
+
+
+Дополнительно
+- TileJSON 3.0.0 / TileJSON [ссылка](https://github.com/mapbox/tilejson-spec/blob/master/3.0.0/README.md)
+- Vector tiles standards / Mapbox [ссылка](https://docs.mapbox.com/data/tilesets/guides/vector-tiles-standards/)
--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://raw.githubusercontent.com/gtitov/basemaps/refs/heads/master/positron-nolabels.json',
-  center: [55.624, 37.584],
+  style: 'https://raw.githubusercontent.com/gtitov/basemaps/refs/heads/master/voyager-nolabels.json',
+  center: [37, 55],
   zoom: 7,
+  maxZoom: 11,
+  maxBounds: [[25, 50], [50, 60]],
   hash: true,
-});
-
+})
 
 map.on('load', () => {
+
+  document.getElementById('filter').addEventListener('input', (e) => {
+    filterValue = parseInt(e.target.value)
+    map.setFilter('grid-layer', ['<', ['to-number', ['get', 'sum_pop']], filterValue])
+  })
 
   map.addSource('grid', {
     type: 'vector',
     url: 'http://localhost:3000/grid',
+    promoteId: 'id'
   })
-
-  map.addSource('oikonyms', {
-    type: 'vector',
-    // url: 'http://localhost:3000/oikonyms',
-    tiles: ['http://localhost:3000/oikonyms/{z}/{x}/{y}'],
-  })
-
   map.addLayer({
     id: 'grid-layer',
     source: 'grid',
@@ -27,16 +27,32 @@ map.on('load', () => {
     type: 'fill',
     paint: {
       'fill-color': [
-        'interpolate', ['linear'],
+        'interpolate',
+        ['linear'],
         ['to-number', ['get', 'sum_pop']],
-        0, '#440154',
-        100, '#39568c',
-        1000, '#1f968b',
-        10000, '#fde725'
+        0,
+        '#440154',
+        100,
+        '#39568c',
+        1000,
+        '#1f968b',
+        10000,
+        '#fde725'
+      ],
+      'fill-outline-color': [
+        'case',
+        ['boolean', ['feature-state', 'hover'], false],
+        'cyan',
+        'transparent'
       ]
     }
   })
 
+  map.addSource('oikonyms', {
+    type: 'vector',
+    tiles: ['http://localhost:3000/oikonyms/{z}/{x}/{y}'],
+    minzoom: 8
+  })
   map.addLayer({
     id: 'oikonyms-layer',
     source: 'oikonyms',
@@ -51,4 +67,75 @@ map.on('load', () => {
     }
   })
 
-});
+  let hoveredFeatureId = null;
+
+  map.on('mousemove', 'grid-layer', (e) => {
+    if (hoveredFeatureId !== null) {
+      map.setFeatureState(
+        {
+          source: 'grid',
+          sourceLayer: 'grid',
+          id: hoveredFeatureId
+        },
+        {hover: false}
+      )
+    }
+    hoveredFeatureId = e.features[0].id
+    map.setFeatureState(
+      {
+        source: 'grid',
+        sourceLayer: 'grid',
+        id: hoveredFeatureId
+      },
+      {hover: true}
+    )
+  })
+
+  map.on('mouseleave', 'grid-layer', () => {
+    map.setFeatureState(
+      {
+        source: 'grid',
+        sourceLayer: 'grid',
+        id: hoveredFeatureId
+      },
+      {hover: false}
+    )
+  })
+
+
+  map.on('click', 'grid-layer', (e) => {
+    map.flyTo({
+      center: e.lngLat,
+      zoom: 10
+    })
+  })
+
+
+  const popup = new maplibregl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  });
+
+  map.on('mouseenter', 'oikonyms-layer', (e) => {
+    popup
+      .setLngLat(e.features[0].geometry.coordinates)
+      .setHTML(e.features[0].properties.name)
+      .addTo(map);
+  });
+
+  map.on('mouseleave', 'oikonyms-layer', () => {
+    popup.remove();
+  });
+
+
+  map.on('mouseenter', 'grid-layer', () => {
+    map.getCanvas().style.cursor = 'pointer'
+  })
+
+  map.on('mouseleave', 'grid-layer', () => {
+    map.getCanvas().style.cursor = ''
+  })
+
+
+  map.addControl(new maplibregl.ScaleControl())
+})
